6b626ad2db1108003fba66368dace093d1d19721
Valentina Vorobyova <vorobyova.valentina2005@gmail.com>
2023-12-13 16:31:32 +0200

    first commit

---
+++ yourfirstapp/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style/footer.css';
+
+const Footer = ({ title }) => {
+  return (
+    <footer className="footer">
+      <h1>{title}</h1>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ yourfirstapp/src/components/sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style/sidebar.css';
+
+const SideBar = ({ title }) => {
+  return (
+    <div class="sidenav">
+      <a href="#">+</a>
+      <a href="#">+</a>
+      <a href="#">+</a>
+      <a href="#">+</a>
+    </div>
+  );
+};
+
+export default SideBar;
